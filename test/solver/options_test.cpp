5ba9f974f0aed45617f9fdba94a0d2371a1b5f81
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2020-10-31 01:58:00 -0700

    Add more options tests

--- test/solver/options_test.cpp
+++ test/solver/options_test.cpp
@@ -1,14 +1,102 @@
-#include "gtest/gtest.h"
-
 #include "../../src/solver/options.hpp"
 
-TEST(OptionsTest, options_contain_value)
-{
-    for (int n = 1; n <= 9; n++) {
-        solver::Options options(n);
+#include <array>
 
-	    for (int m = 1; m <= 9; m++) {
+#include "gtest/gtest.h"
+
+TEST(OptionsTest, contains_single) {
+    for (int n = 1; n <= 9; n++) {
+        const solver::Options options(n);
+
+        for (int m = 1; m <= 9; m++) {
             EXPECT_EQ(n == m, options.contains(m));
         }
     }
+}
+
+TEST(OptionsTest, solved_single) {
+    solver::Options all;
+    EXPECT_EQ(false, all.solved());
+
+    for (int n = 1; n <= 9; n++) {
+        const solver::Options single(n);
+
+        EXPECT_EQ(true, single.solved());
+    }
+}
+
+TEST(OptionsTest, value_accessor) {
+    solver::Options all;
+    EXPECT_EQ(0, all.value());
+
+    for (int n = 1; n <= 9; n++) {
+        const solver::Options options(n);
+
+        EXPECT_EQ(n, options.value());
+    }
+}
+
+TEST(OptionsTest, remove_ordered) {
+    solver::Options options;
+
+    for (int n = 1; n <= 8; n++) {
+        EXPECT_EQ(false, options.solved());
+        EXPECT_EQ(0, options.value());
+        EXPECT_EQ(true, options.contains(n));
+
+        options -= n;
+
+        EXPECT_EQ(false, options.contains(n));
+    }
+
+    EXPECT_EQ(true, options.solved());
+    EXPECT_EQ(9, options.value());
+}
+
+TEST(OptionsTest, remove_disordered) {
+    std::array<int, 8> values { 3, 1, 7, 4, 5, 9, 8, 2 };
+
+    solver::Options options;
+
+    for (int n : values) {
+        EXPECT_EQ(false, options.solved());
+        EXPECT_EQ(0, options.value());
+        EXPECT_EQ(true, options.contains(n));
+
+        options -= n;
+
+        EXPECT_EQ(false, options.contains(n));
+    }
+
+    EXPECT_EQ(true, options.solved());
+    EXPECT_EQ(6, options.value());
+}
+
+TEST(OptionsTest, combine) {
+    solver::Options options;
+    options -= 7;
+    options -= 3;
+    options -= 5;
+    options -= 2;
+
+    solver::Options other;
+    options -= 1;
+    options -= 2;
+    options -= 4;
+    options -= 6;
+    options -= 9;
+
+    options &= other;
+
+    EXPECT_EQ(false, options.contains(1));
+    EXPECT_EQ(false, options.contains(2));
+    EXPECT_EQ(false, options.contains(3));
+    EXPECT_EQ(false, options.contains(4));
+    EXPECT_EQ(false, options.contains(5));
+    EXPECT_EQ(false, options.contains(6));
+    EXPECT_EQ(false, options.contains(7));
+    EXPECT_EQ(false, options.contains(9));
+
+    EXPECT_EQ(true, options.solved());
+    EXPECT_EQ(8, options.value());
 }
